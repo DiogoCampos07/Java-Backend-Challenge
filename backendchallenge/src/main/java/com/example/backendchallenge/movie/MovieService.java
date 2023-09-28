845a99344901bfca7dd0231d35d1b4f84a3a0b63
Diogo Campos <83606982+DiogoCampos07@users.noreply.github.com>
2023-09-28 22:59:10 +0100

    Movie Service

---
+++ backendchallenge/src/main/java/com/example/backendchallenge/movie/MovieService.java
@@ -0,0 +1,75 @@
+package com.example.backendchallenge.movie;
+
+import com.example.backendchallenge.exceptions.MovieNotFoundException;
+import com.example.backendchallenge.movie.dto.MovieDTO;
+import jakarta.validation.*;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class MovieService {
+
+    private final MovieRepository repository;
+
+    public MovieService(MovieRepository repository) {
+        this.repository = repository;
+    }
+
+    private void validateAndSaveMovie(Movie movie) {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<Movie>> violations = validator.validate(movie);
+
+        if (!violations.isEmpty()) {
+            StringBuilder errorMessage = new StringBuilder();
+            for (ConstraintViolation<Movie> violation : violations) {
+                errorMessage.append(violation.getMessage()).append(";");
+            }
+            throw new ValidationException(errorMessage.toString());
+        }
+
+        repository.save(movie);
+    }
+
+    public Movie createMovie(MovieDTO movieDTO) {
+        Movie newMovie = new Movie(movieDTO);
+        validateAndSaveMovie(newMovie);
+        return newMovie;
+    }
+
+    public Movie getMovieById(UUID id) {
+        return repository.findById(id).orElseThrow(() -> new MovieNotFoundException(id));
+    }
+
+    public List<Movie> getAllMovies() {
+        return repository.findAll();
+    }
+
+    public Movie updateMovie(UUID id, MovieDTO movieDTO) {
+        Movie movie = repository.findById(id).orElseThrow(() -> new MovieNotFoundException(id));
+        movie.setTitle(movieDTO.title());
+        movie.setLaunchDate(movieDTO.launchDate());
+        movie.setRank(movieDTO.rank());
+        movie.setRevenue(movieDTO.revenue());
+        if (movie.getRank() == null || movie.getRevenue() == null) {
+            throw new ValidationException("Rank and/or revenue cannot be null.");
+        }
+        validateAndSaveMovie(movie);
+        repository.save(movie);
+        return movie;
+    }
+
+    public void deleteById(UUID id) {
+        repository.findById(id).orElseThrow(() -> new MovieNotFoundException(id));
+        repository.deleteById(id);
+    }
+
+    public List<Movie> getFilteredMovies(LocalDate startDate, LocalDate endDate) {
+        return repository.getFilteredMovies(startDate, endDate);
+    }
+
+}
