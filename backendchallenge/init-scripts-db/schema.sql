e2a53494a0cb6183b6290d6ff0f32c80bd7505f8
Diogo Campos <83606982+DiogoCampos07@users.noreply.github.com>
2023-09-30 12:58:39 +0100

    script error fix in uuid auto generate

--- backendchallenge/init-scripts-db/schema.sql
+++ backendchallenge/init-scripts-db/schema.sql
@@ -5,9 +5,9 @@ create extension if not exists "uuid-ossp";
 --- TABLES
 
 CREATE TABLE movies (
-                        id UUID PRIMARY KEY NOT NULL,
+                        id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
                         title VARCHAR(250) NOT NULL CHECK (LENGTH(title) >= 2 AND LENGTH(title) <= 250),
-                        launch_date DATE NOT NULL,  -- Change to launch_date
+                        launch_date DATE NOT NULL,
                         rank INTEGER NOT NULL CHECK (rank >= 0 AND rank <= 10),
                         revenue NUMERIC(19, 2) NOT NULL
 );
